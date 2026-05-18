6abfb48c6b3f1126c2ab5c8d51603053eaf8747c
raffibromabar-web <raffibromabar@gmail.com>
2026-05-18 20:24:28 +0700

    Menggabungkan pekerja dan pelajar ke karakter Budi

--- solvemultiwaris.cpp
+++ solvemultiwaris.cpp
@@ -50,3 +50,21 @@ cout << "Pelajar dibuat\n" << endl;
 }
 
 };
+
+class budi : public pekerja, public pelajar {
+
+public:
+
+budi(int pUmur) :
+
+pekerja(pUmur),
+
+pelajar(pUmur)
+
+{
+
+cout << "Budi dibuat\n" << endl;
+
+}
+
+};

a88fe3b36a049ad3f1a9aea71ee787918239f010
raffibromabar-web <raffibromabar@gmail.com>
2026-05-18 20:04:00 +0700

    menambahkan class pekerja

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -19,3 +19,18 @@ cout << "orang dibuat dengan umur " << umur << "\n" << endl;
 
 }
 };
+
+class pekerja : virtual public orang {
+
+public:
+
+pekerja(int pUmur) :
+
+orang(pUmur)
+
+{
+cout << "pekerja dibuat\n" << endl;
+}
+
+};
+

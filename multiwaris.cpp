95682dd6096cc10b9cffcb2336f91bf5df0b67ea
raffibromabar-web <raffibromabar@gmail.com>
2026-05-18 20:05:30 +0700

    menambahkan milik budi

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -49,3 +49,25 @@ cout << "pelajar dibuat\n" << endl;
 }
 
 };
+
+class budi : public pekerja, public pelajar {
+
+public:
+
+
+
+budi(int pUmur) :
+
+pekerja(pUmur),
+
+pelajar(pUmur),
+
+orang(pUmur) //hal ini dapat dilakukan jika menggunakan virtual
+
+{
+
+cout << "Budi dibuat\n" << endl;
+
+}
+
+};

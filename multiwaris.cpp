30b43a745d9a806171d3fb1f078bbbe16181071c
raffibromabar-web <raffibromabar@gmail.com>
2026-05-18 20:04:37 +0700

    menambahkan pelajar

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -34,3 +34,18 @@ cout << "pekerja dibuat\n" << endl;
 
 };
 
+class pelajar : virtual public orang {
+
+public:
+
+pelajar(int pUmur) :
+
+orang(pUmur)
+
+{
+
+cout << "pelajar dibuat\n" << endl;
+
+}
+
+};
